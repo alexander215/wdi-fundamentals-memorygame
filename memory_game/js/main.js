20efac1f3e50fa19211bb63c168ac0e86652695a
Alexander Hoskinson <alexanderhoskinson@Alexanders-MacBook-Pro.local>
2019-06-09 17:26:26 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,17 +28,28 @@ function checkForMatch() {
 		alert("Sorry, try again.");
 	}
 }
-function flipCard(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	//console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
-if (cardsInPlay.length === 2) {
-	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
+
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 }
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
